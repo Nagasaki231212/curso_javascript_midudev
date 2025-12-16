28321a587f0570a0f5807835ad8c79a818b62837
Oscar Gil <mcastrorap@gmail.com>
2025-12-16 12:37:51 -0400

    como utilizar correctamente console.log

---
+++ consolelog.js
@@ -0,0 +1,14 @@
+/*puedes averiguar el valor de una variable, escribiendo el nombre de la variable dentro de los paréntesis.
+se imprime en la consola con el comando node seguido el nombre del archivo. node consolelog*/
+
+const nombre = 'JavaScript'
+console.log(nombre)
+// esto imprime JavaScript
+
+
+/*Más métodos de console
+Además de console.log(), existen otros métodos que puedes utilizar para imprimir mensajes en la consola. Algunos de ellos son:
+
+console.error(): Imprime un mensaje de error en la consola.
+console.warn(): Imprime un mensaje de advertencia en la consola.
+console.info(): Imprime un mensaje de información en la consola.*/
