ab2273864a75fc67cdbaceb08a73da4654c554cb
Oscar Gil <mcastrorap@gmail.com>
2025-12-16 11:13:15 -0400

    variables

---
+++ variables.js
@@ -0,0 +1,36 @@
+let number = 2 // let para definifir una variable reutilizable
+
+console.log(number + 2) // console.log para imprimir en la terminal siempre con parentesis el resultado que queremos imprimir console.log(..)
+
+number = 4 
+
+number = number + 6 
+
+console.log(number)
+
+let text 
+
+text = "Hola mundo"
+
+console.log(text)
+
+
+// una variable se le puede asignar otra variable 
+
+let numberOne = 1
+
+let numberTwo = numberOne
+
+console.log(numberTwo)
+
+
+
+// variables con Booleanos 
+
+let isUserLogged = true 
+
+// Las constantes son variables que no pueden ser reasignadas. Para crear una constante, se usa  la palabra reservada const:
+
+const booleano = true 
+
+console.log(booleano = false) // esto da error en la consola porque las variables constantes no se pueden reutilizar.
