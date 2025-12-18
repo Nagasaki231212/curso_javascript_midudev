598929a714fcddb0cfce1c5de5df11fdec41d965
Oscar Gil <mcastrorap@gmail.com>
2025-12-18 12:11:16 -0400

    clase de do_while

---
+++ do_while.js
@@ -0,0 +1,24 @@
+// Aunque no es muy famoso ni muy utilizado, es interesante que sepas que existe en JavaScript un bucle que se ejecuta al menos una vez,
+// y luego se repite mientras se cumpla una condición. Este bucle se llama do while y tiene la siguiente sintaxis:
+
+console.log("Ejercicio uno")
+
+let contar = 1 
+
+do {
+    contar = contar - 1 
+    console.log(contar)
+} while (contar > 10) 
+
+
+console.log("Ejercicio dos")
+
+let count = 20 
+
+do {
+    count = count - 2
+    console.log(count)
+} while (count > 14) 
+
+console.log("Ejercicio tres")
+
